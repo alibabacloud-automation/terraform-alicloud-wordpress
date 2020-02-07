eb126a62ee0eddc5dbca39a207a398b216c33dea
He Guimin <guimin.hgm@alibaba-inc.com>
2020-02-07 14:27:54 +0800

    rename example and add annotatins for examples

--- examples/install-on-ecs-and-rds/main.tf
+++ examples/install-on-ecs-and-rds/main.tf
@@ -4,6 +4,9 @@ variable "region" {
 provider "alicloud" {
   region = var.region
 }
+##################################################################
+# Data sources to get VPC, vswitch, ecs instance types and rds instance classes details
+##################################################################
 data "alicloud_vpcs" "default" {
   is_default = true
 }
@@ -14,6 +17,11 @@ data "alicloud_db_instance_classes" "default" {
   zone_id        = data.alicloud_vswitches.default.vswitches.0.zone_id
   engine         = "MySQL"
   engine_version = "5.7"
+}
+data "alicloud_instance_types" "this" {
+  cpu_core_count    = 1
+  memory_size       = 2
+  availability_zone = data.alicloud_vswitches.default.vswitches.0.zone_id
 }
 module "wordpress" {
   source = "../../"
@@ -31,12 +39,16 @@ module "wordpress" {
   ###########
   ecs_instance_name          = "myDBInstance3"
   ecs_instance_password      = "YourPassword123"
-  ecs_instance_type          = "ecs.sn1ne.large"
+  ecs_instance_type          = data.alicloud_instance_types.this.ids.0
   system_disk_category       = "cloud_efficiency"
   security_group_ids         = [module.security_group.this_security_group_id]
   vswitch_id                 = data.alicloud_vpcs.default.vpcs.0.vswitch_ids.0
   internet_max_bandwidth_out = 50
 }
+
+##################################################################
+# Create a new security group using terraform module
+##################################################################
 module "security_group" {
   source              = "alibaba/security-group/alicloud"
   region              = var.region
